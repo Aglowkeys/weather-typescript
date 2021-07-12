8affc7cdd07fc8524a5e06de688863b7b43ce817
Aglowkeys <emilianoalfonso@outlook.com>
2021-07-11 22:04:30 -0300

    Added github icon

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ const App = () => {
 		<div className='App'>
 			<Header />
 			{!error && city.name && <City />}
+			<div className='icon-github'>
+				<a
+					href='https://github.com/Aglowkeys/weather-typescript'
+					target='_blank'
+					rel='noopener noreferrer'
+					aria-label='Visitar repositorio en Github'
+				>
+					<i className='fab fa-github'></i>
+				</a>
+			</div>
 		</div>
 	);
 };
